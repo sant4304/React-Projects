467cf05b7f7a79c3e827f0d82af6a2b4e49ef5db
sant4304 <santoshsk.3304.sk@gmail.com>
2026-06-17 20:09:55 +0530

    Fix class4 folder

---
+++ class4/src/component/Women.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Women = () => {
+  return (
+   <div className='h-20 w-fit bg-pink-600 rounded px-5 py-2 m-2'>
+      <h1 className='text-xl font-bold'>Women Washroom</h1>
+    </div>
+  )
+}
+
+export default Women
